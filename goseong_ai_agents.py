b034e37aebf6adc4953097646e41d9c72f88c745
ds2rnz <ds2rnz@naver.com>
2025-11-04 11:31:43 +0900

    Add files via upload

--- goseong_ai_agents.py
+++ goseong_ai_agents.py
@@ -90,7 +90,7 @@ if prompt := st.chat_input("무엇이든 물어보세요!"):
         # agent.stream()을 사용하여 실시간 출력
         for event in agent.stream(inputs, stream_mode="updates"):
             st.write(event.items())
-            event_text = event.items(AIMessage)
+            event_text = event.items({'model':{'messages':[AIMessage(content=event)]}})
             st.write(event_text)
             # event_text1 = {event_text['messages'][-1].content_blocks}
             st.write(event_text)
