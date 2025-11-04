902bcd2488c77632b045eef63103e0594e0147f8
ds2rnz <ds2rnz@naver.com>
2025-11-04 11:09:25 +0900

    Add files via upload

--- goseong_ai_agents.py
+++ goseong_ai_agents.py
@@ -88,7 +88,7 @@ if prompt := st.chat_input("무엇이든 물어보세요!"):
         streamed_text = ""
 
         # agent.stream()을 사용하여 실시간 출력
-        for event in agent.stream(inputs, stream_mode="messages"):
+        for event in agent.stream(inputs, stream_mode="updates"):
             event_text = event["messages"][-1]
             st.write(f"content: {event}")
             st.write("\n")
