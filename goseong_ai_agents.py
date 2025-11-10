b06776fb4d9688776edba9cd39dd8688b1fe8bba
ds2rnz <ds2rnz@naver.com>
2025-11-10 17:19:20 +0900

    Update goseong_ai_agents.py

--- goseong_ai_agents.py
+++ goseong_ai_agents.py
@@ -38,7 +38,8 @@ def get_current_time(timezone: str, location: str) -> str:
         return f"알 수 없는 타임존: {timezone}"  
     
 @tool
-def search_searx(query):
+def search_searx(query) -> str:
+    ''' searx를 활용한 인터넷 검색 툴'''
     # Searx 인스턴스 URL
     searx_url = "https://searx.org/search"  # 공개된 Searx 인스턴스 URL
     params = {
