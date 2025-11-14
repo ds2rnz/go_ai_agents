613751c18514385023bf507912beec9bed20c603
ds2rnz <ds2rnz@naver.com>
2025-11-14 16:20:38 +0900

    Update config.py

--- config.py
+++ config.py
@@ -2,11 +2,13 @@ import streamlit as st
 import os
 from langchain_openai import OpenAIEmbeddings
 
-
-def embedding():
-    OpenAIEmbeddings(
+@st.cache_resource
+def get_embedding():
+    """Embedding 모델 반환 (캐시됨)"""
+    return OpenAIEmbeddings(
         model="text-embedding-3-large", 
         api_key=st.secrets.get("OPENAI_API_KEY", os.getenv("OPENAI_API_KEY"))
-        )
+    )
+
 
 
