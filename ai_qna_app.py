2be2041f8dbe77d534dfd0101f34e6f96a78bdc1
ds2rnz <ds2rnz@naver.com>
2025-11-14 11:22:47 +0900

    Update ai_qna_app.py

--- ai_qna_app.py
+++ ai_qna_app.py
@@ -10,7 +10,7 @@ from pathlib import Path
 import tempfile
 import traceback
 import time
-from config import llm, agent
+from config import llm, agent, config
 
 
 
@@ -169,6 +169,7 @@ def process1_f(uploaded_files1):
         st.error(f"❌ 학습 중 오류 발생: {e}")
         st.code(traceback.format_exc(), language="python")
         return None
+
 
 
 
