45e6c448007f8f76b23f57505c49cdc88a836dc7
ds2rnz <ds2rnz@naver.com>
2025-11-16 12:10:29 +0900

    Update ai_qna_app.py

--- ai_qna_app.py
+++ ai_qna_app.py
@@ -69,7 +69,7 @@ system_prompt_text = """
 """
 
 llm = init_chat_model(
-    model = "openai:gpt-5",
+    model = "openai:gpt-4o-mini",
     temperature=0.6, 
     max_tokens=1500, 
     timeout=15, 
@@ -246,6 +246,7 @@ def process1_f(uploaded_files1):
         st.error(f"❌ 학습 중 오류 발생: {e}")
         st.code(traceback.format_exc(), language="python")
         return None
+
 
 
 
