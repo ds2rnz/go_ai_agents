9448187618619c6b43495d2683bf11e189094001
ds2rnz <ds2rnz@naver.com>
2025-11-19 17:27:00 +0900

    Update ai_qna_app.py

--- ai_qna_app.py
+++ ai_qna_app.py
@@ -74,19 +74,12 @@ system_prompt_text = """
 9. 한글로 답해주세요
 """
 
-#llm = init_chat_model(
- #   model = "openai:gpt-4o",
-  #  temperature=0.6, 
-   # max_tokens=1000, 
-    # timeout=30, 
-    # max_retries=2, 
-    # )
-
-llm = ChatOpenAI(
-    model = "gpt-4o",
+llm = init_chat_model(
+    model = "openai:gpt-4o",
     temperature=0.6, 
     max_tokens=1000, 
     timeout=20, 
+    max_retries=2, 
     )
 
 
@@ -260,6 +253,7 @@ def process1_f(uploaded_files1):
         st.error(f"❌ 학습 중 오류 발생: {e}")
         st.code(traceback.format_exc(), language="python")
         return None
+
 
 
 
