1548bc1e0335f82423fe98f910200f7356a60e60
ds2rnz <ds2rnz@naver.com>
2025-11-20 16:13:17 +0900

    Update ai_qna_app.py

--- ai_qna_app.py
+++ ai_qna_app.py
@@ -19,37 +19,37 @@ from langchain.chat_models import init_chat_model
 from langchain_openai import ChatOpenAI
 from langchain.agents import create_agent
 
-@tool
-def get_current_time(timezone: str, location: str) -> str:
-    '''  해당 지역 현재시각을 구하는 함수 '''
-    try:
-        tz = pytz.timezone(timezone)
-        now = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
-        result = f'{timezone} ({location}) 현재시각 {now}'
-        return result
-    except pytz.UnknownTimeZoneError:
-        return f"알 수 없는 타임존: {timezone}"  
+# @tool
+# def get_current_time(timezone: str, location: str) -> str:
+#     '''  해당 지역 현재시각을 구하는 함수 '''
+#     try:
+#         tz = pytz.timezone(timezone)
+#         now = datetime.now(tz).strftime("%Y-%m-%d %H:%M:%S")
+#         result = f'{timezone} ({location}) 현재시각 {now}'
+#         return result
+#     except pytz.UnknownTimeZoneError:
+#         return f"알 수 없는 타임존: {timezone}"  
 
-@tool
-def get_web_search(query: str) -> str:
-    """
-    웹 검색을 수행하는 함수.
+# @tool
+# def get_web_search(query: str) -> str:
+#     """
+#     웹 검색을 수행하는 함수.
 
-    Args:
-        query (str): 검색어
-    Returns:
-        str: 검색 결과
-    """
-    custom_wrapper = DuckDuckGoSearchAPIWrapper(region="kr-kr", time="y", max_results=10)
-    search = DuckDuckGoSearchResults(
-        api_wrapper=custom_wrapper,
-        source="news, image, text",
-        results_separator=';\n')
+#     Args:
+#         query (str): 검색어
+#     Returns:
+#         str: 검색 결과
+#     """
+#     custom_wrapper = DuckDuckGoSearchAPIWrapper(region="kr-kr", time="y", max_results=10)
+#     search = DuckDuckGoSearchResults(
+#         api_wrapper=custom_wrapper,
+#         source="news, image, text",
+#         results_separator=';\n')
     
-    results = search.run(query)
+#     results = search.run(query)
 
-    st.toast("웹 검색을 통아여 알아보고 있습니다.", icon="🎉")
-    return results
+#     st.toast("웹 검색을 통아여 알아보고 있습니다.", icon="🎉")
+#     return results
 
 
 
@@ -91,7 +91,7 @@ embedding = OpenAIEmbeddings(
 
 agent = create_agent(
     model=llm,
-    tools=[get_current_time, get_web_search],
+    tools=[],
     middleware=[],
     system_prompt=system_prompt_text, 
     )
@@ -253,6 +253,7 @@ def process1_f(uploaded_files1):
         st.error(f"❌ 학습 중 오류 발생: {e}")
         st.code(traceback.format_exc(), language="python")
         return None
+
 
 
 
